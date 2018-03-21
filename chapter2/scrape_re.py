7f0477a526df062e1108b0656e5d3b4e37dd5739
nao37 <watanabenao@watanabenaonoMacBook-Pro-2.local>
2018-03-22 02:19:52 +0900

    scraping_re.py

---
+++ chapter2/scrape_re.py
@@ -0,0 +1,17 @@
+import re
+from html import unescape
+
+with open('dp.html') as f:
+    html = f.read()
+
+for partial_html in re.findall(r'<a itemprop="url".*?</ul>\s*</a></li>', html, re.DOTALL):
+    url = re.search(r'<a itemprop="url" href="(.*?)">', partial_html, re.DOTALL).group(1)
+    url = 'html://sample.scraping-book.com' + url
+
+    title = re.search(r'<p itemprop="name".*?</p>', partial_html).group(0)
+    title = re.sub(r'<br/>', ' ', title)
+    title = re.sub(r'<.*?>', '', title)
+    # title = re.sub(r'&amp;', '&', title)
+    title = unescape(title)
+
+    print(url, title)
